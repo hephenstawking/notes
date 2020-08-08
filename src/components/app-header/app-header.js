995a6e89a0ae3719bd53b9aca9fe971ed5d3683c
Dmytro <sevenfofive@outlook.com>
2020-08-08 15:59:28 +0300

    first commit

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AppHeader = () => {
+    return (
+        <div className="app-header d-flex">
+            <h1>Dmytro Korsakov</h1>
+            <h2>5 records, 0 liked</h2>
+        </div>
+    )
+}
+
+export default AppHeader;
